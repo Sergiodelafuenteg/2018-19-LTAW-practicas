09773341a4af9dfaaae90377e30e24ff3872f4e5
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-01-31 20:01:22 +0100

    init stream y pipe

--- Practica-1/library-server.js
+++ Practica-1/library-server.js
@@ -2,11 +2,10 @@
  const fs = require('fs')
 
  http.createServer( (req,res) => {
-     fs.readFile('library.html', (err, index) => {
-         res.writeHead(200, { 'Content-Type': 'text/html' });
-         res.write(index);
-         res.end();
-     });
+     console.log('req was: ' + req.url);
+     res.writeHead(200, { "Content-Type": "text/html" });
+     var stream = fs.createReadStream('library.html', 'utf8');
+     stream.pipe(res);
  }
  ).listen(8080)
  
