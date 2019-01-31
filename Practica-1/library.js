2193563696194e14096d14588d132e5de39d1709
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-01-31 21:56:41 +0100

    HttpRequest y JSON

--- Practica-1/library.js
+++ Practica-1/library.js
@@ -1,3 +1,12 @@
-function Prueba(params) {
-    return 4;
+
+function PeticionJSON(params) {
+    var www = new XMLHttpRequest();
+    www.open("GET", "https://randomuser.me/api/?results=5", false);
+    www.send()
+    console.log(www.responseType);
+    var people = JSON.parse(www.responseText);
+    console.log(people);
+    
+    document.getElementById("prueba").innerHTML = people.results[2].email;
+    
 }
