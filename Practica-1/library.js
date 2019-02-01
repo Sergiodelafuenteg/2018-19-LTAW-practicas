624d776ea53c0e8f8f201816d7051850c8c86dbb
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-02-01 19:53:28 +0100

    luchando con el css

--- Practica-1/library.js
+++ Practica-1/library.js
@@ -11,17 +11,30 @@ function PeticionJSON(gender) {
     // creamos dos subgrupos mujeres y hombres
     var gender_array = people.results.filter(person => person.gender == gender);
     console.log(gender_array);
+    document.getElementById("prueba").innerHTML = "";
     // Montamos el array de "productos"
     gender_array.forEach(user => {
-        let prod = document.createElement("ul");
+        let productos = document.createElement("ul");
+        let prod = document.createElement("div");
         let image = document.createElement("img");
+        let text_name = document.createElement("span");
+        
+        let text_price = document.createElement("span");
+                
         let name = document.createTextNode('  ' + user.name.first);
-        // name.setAttribute("class","name");
-        let price = document.createTextNode('  price: ' + user.dob.age + '€')
-        image.src = user.picture.thumbnail;
+        let price = document.createTextNode(user.dob.age + '€')
+        text_name.appendChild(name);
+        text_price.appendChild(price);
+        image.src = user.picture.large;
+        image.style.textAlign = "left";
         prod.appendChild(image);
-        prod.appendChild(name);
-        prod.appendChild(price);
-        document.getElementById('prueba').appendChild(prod);    
+        image.setAttribute("class", "image");
+        text_name.setAttribute("class", "name");
+        text_price.setAttribute("class", "price");
+        prod.appendChild(text_name);
+        prod.appendChild(text_price);
+        prod.setAttribute("class", "prod")        
+        productos.appendChild(prod);
+        document.getElementById('prueba').appendChild(productos);    
     });
 }
