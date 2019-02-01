a6adeb791ef01cce9e9147ce171d864f8253c3cb
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-02-01 17:51:52 +0100

    Falta CSS

--- Practica-1/library.js
+++ Practica-1/library.js
@@ -1,13 +1,27 @@
 
 function PeticionJSON(gender) {
+    // manejamos la peticion para la "tienda"
+    // usamos el api randomuser por comodidad
     var request = new XMLHttpRequest();
-    request.open("GET", "https://randomuser.me/api/?results=5", false);
+    request.open("GET", "https://randomuser.me/api/?results=50", false);
     request.send()
     console.log(request.responseType);
     var people = JSON.parse(request.responseText);
     console.log(people);
+    // creamos dos subgrupos mujeres y hombres
     var gender_array = people.results.filter(person => person.gender == gender);
     console.log(gender_array);
-    document.getElementById("prueba").innerHTML = gender_array;
-    
+    // Montamos el array de "productos"
+    gender_array.forEach(user => {
+        let prod = document.createElement("ul");
+        let image = document.createElement("img");
+        let name = document.createTextNode('  ' + user.name.first);
+        // name.setAttribute("class","name");
+        let price = document.createTextNode('  price: ' + user.dob.age + '€')
+        image.src = user.picture.thumbnail;
+        prod.appendChild(image);
+        prod.appendChild(name);
+        prod.appendChild(price);
+        document.getElementById('prueba').appendChild(prod);    
+    });
 }
