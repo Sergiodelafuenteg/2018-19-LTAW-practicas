4d2f59120bfdd1bfddcc253f63c369bbbcf8d20b
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-02-01 16:20:24 +0100

    montado el filtro para el genero

--- Practica-1/library.js
+++ Practica-1/library.js
@@ -1,12 +1,13 @@
 
-function PeticionJSON(params) {
+function PeticionJSON(gender) {
     var request = new XMLHttpRequest();
     request.open("GET", "https://randomuser.me/api/?results=5", false);
     request.send()
     console.log(request.responseType);
     var people = JSON.parse(request.responseText);
     console.log(people);
-    
-    document.getElementById("prueba").innerHTML = people.results[2].email;
+    var gender_array = people.results.filter(person => person.gender == gender);
+    console.log(gender_array);
+    document.getElementById("prueba").innerHTML = gender_array;
     
 }
