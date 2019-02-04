8714ee870d744ddb3cbb2b5bf969234e0150f35c
Sergio  De La Fuente Garcia <sfuente@delta36.aulas.gsyc.urjc.es>
2019-02-04 12:28:51 +0100

    External files solucionado

--- Practica-1/library-server.js
+++ Practica-1/library-server.js
@@ -3,17 +3,32 @@ const fs = require('fs')
 const path = require('path')
 
 http.createServer((req,res) => {
-     console.log('req was: ' + req.url.split('.')[1]);
-     if (req.url.split(".")[1] == 'js') {
-         console.log('javascript');
-         res.writeHead(200, { "Content-Type": "text/javascript" });
-         let js_path = path.join(__dirname, req.url)
-         var stream = fs.createReadStream('library.js');
-         stream.pipe(res);    
+     console.log('req was: ' + req.url);
+    if (req.url == '/') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        var stream = fs.createReadStream('library.html', 'utf8');
+        stream.pipe(res);
      }
-     res.writeHead(200, { "Content-Type": "text/html" });
-     var stream = fs.createReadStream('library.html', 'utf8');
-     stream.pipe(res);
+
+    if (req.url.split(".")[1] == 'js') {
+        console.log('javascript');
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        let js_path = path.join(__dirname, req.url)
+        console.log(js_path);
+        
+        var stream = fs.createReadStream(js_path);
+        stream.pipe(res);    
+    }
+    if (req.url.split(".")[1] == 'css') {
+        console.log('css');
+        res.writeHead(200, { "Content-Type": "text/css" });
+        let css_path = path.join(__dirname, req.url)
+        console.log(css_path);
+
+        var stream = fs.createReadStream(css_path);
+        stream.pipe(res);
+    }
+     
 }
 ).listen(8080)
 
