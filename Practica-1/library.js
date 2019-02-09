a2a34ad58d23e0ad5a2480f21f5a827b04cc3a30
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-02-09 12:24:14 +0100

    limpiando

--- Practica-1/library.js
+++ Practica-1/library.js
@@ -14,25 +14,29 @@ function PeticionJSON(gender) {
     document.getElementById("visor").innerHTML = ""
     // Montamos el array de "productos"
     gender_array.forEach(user => {
+        // Creamos los elementos
         let productos = document.createElement("ul");
         let prod = document.createElement("div");
         let image = document.createElement("img");
-        let text_name = document.createElement("span");
-        
-        let text_price = document.createElement("span");
-                
+        let text_name = document.createElement("span");        
+        let text_price = document.createElement("span");        
+        let butt_add = document.createElement("button");                
         let name = document.createTextNode('  ' + user.name.first);
         let price = document.createTextNode(user.dob.age + '€')
+        butt_add.innerHTML = "+";
+        // Los incorporamos en el DOM
         text_name.appendChild(name);
         text_price.appendChild(price);
         image.src = user.picture.large;
-        image.style.textAlign = "left";
+        // image.style.textAlign = "left";
         prod.appendChild(image);
         image.setAttribute("class", "image");
         text_name.setAttribute("class", "name");
         text_price.setAttribute("class", "price");
+        butt_add.setAttribute("class", "butt_add");
         prod.appendChild(text_name);
         prod.appendChild(text_price);
+        prod.appendChild(butt_add);
         prod.setAttribute("class", "prod")        
         productos.appendChild(prod);
         document.getElementById('visor').appendChild(productos);    
