06f35e03c6736cde9ec80db95dd791c403fcf2cb
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2019-04-15 03:15:27 +0200

    terninando caja de busqueda

--- Practica-3/tienda/library.js
+++ Practica-3/tienda/library.js
@@ -1,69 +1,71 @@
 
 var Cesta = [];
 
-function PeticionJSON(Categor) {
-    // manejamos la peticion para la "tienda"
-    // usamos el api randomuser por comodidad
+function CargarJsonLocal(file) {
+  let request = new XMLHttpRequest();
+  request.open("GET", file, false);
+  request.send();
+  console.log(request.responseType);
+  let JSON_results = JSON.parse(request.responseText);
+  return JSON_results;
+}
+
+// funcion para cargar los productos categorizados
+function Load_Prod(Categor) {
+ 
     document.getElementById('box').style.display = 'inline-block';
-    find();
-    var request = new XMLHttpRequest();
-    request.open("GET", "products.json", false);
-    request.send()
-    console.log(request.responseType);
-    var results = JSON.parse(request.responseText);
-    console.log(Object.keys(results)[2]);
     // creamos dos subgrupos mujeres y hombres
+    var results = CargarJsonLocal("products.json")
     var Categor_array = results[Categor]
     console.log(Categor_array);
+    // ponemos visor en blanco. Listo para insertar
     document.getElementById("visor").innerHTML = ""
-    // añadimos handler para caja de busqueda
-    function find() {
-        var box = document.getElementById('box');
-        box.addEventListener('keydown', (event) => {
-            var presskey = event.key;
-            console.log(presskey);
-            Categor_array = results.filter(obj => obj.name[0] == presskey);
-            console.log(Categor_array);
-        })
-    }
+
     // Montamos el array de "productos"
-    Categor_array.forEach(user => {
+    Categor_array.forEach(item => {
         // Creamos los elementos
-        let productos = document.createElement("ul");
-        let prod = document.createElement("div");
-        let image = document.createElement("img");
-        let text_name = document.createElement("span");        
-        let text_price = document.createElement("span");        
-        let butt_add = document.createElement("button");                
-        let name = document.createTextNode('  ' + user.name);
-        let price = document.createTextNode(user.price + '€')
-        butt_add.innerHTML = "+";
-        // Los incorporamos en el DOM
-        text_name.appendChild(name);
-        text_price.appendChild(price);
-        image.src = user.image;
-        // image.style.textAlign = "left";
-        prod.appendChild(image);
-        image.setAttribute("class", "image");
-        text_name.setAttribute("class", "name");
-        text_price.setAttribute("class", "price");
-        butt_add.setAttribute("class", "butt_add");
-        butt_add.setAttribute("onclick", "Abrircesta()");
-        prod.appendChild(text_name);
-        prod.appendChild(text_price);
-        prod.appendChild(butt_add);
-        prod.setAttribute("class", "prod")        
-        productos.appendChild(prod);
-        document.getElementById('visor').appendChild(productos);
+        Push_Product(item);
         // Creamos un objeto producto para el array
         var prod_cesta = {
-            name : user.name,
-            image : user.image,
-            precio : user.price
+            name : item.name,
+            image : item.image,
+            precio : item.price
         }
         Cesta.push(prod_cesta)
     });
 }
+
+// funcion para insertar los elementos en el DOM
+function Push_Product(item) {
+    let productos = document.createElement("ul");
+    let prod = document.createElement("div");
+    let image = document.createElement("img");
+    let text_name = document.createElement("span");
+    let text_price = document.createElement("span");
+    let butt_add = document.createElement("button");
+    let name = document.createTextNode("  " + item.name);
+    let price = document.createTextNode(item.price + "€");
+    butt_add.innerHTML = "+";
+    // Los incorporamos en el DOM
+    text_name.appendChild(name);
+    text_price.appendChild(price);
+    image.src = item.image;
+    // image.style.textAlign = "left";
+    prod.appendChild(image);
+    image.setAttribute("class", "image");
+    text_name.setAttribute("class", "name");
+    text_price.setAttribute("class", "price");
+    butt_add.setAttribute("class", "butt_add");
+    butt_add.setAttribute("onclick", "Abrircesta()");
+    prod.appendChild(text_name);
+    prod.appendChild(text_price);
+    prod.appendChild(butt_add);
+    prod.setAttribute("class", "prod");
+    productos.appendChild(prod);
+    document.getElementById("visor").appendChild(productos);
+}
+
+
 function Abrircesta() {
     console.log(Cesta);
     
@@ -71,29 +73,26 @@ function Abrircesta() {
     image.src = "cesta1.png";
 
 }
-function CargarJsonLocal(params) {
-    var hu = 'Bicicletas'
-    var request2 = new XMLHttpRequest();
-    request2.open("GET", "products.json", false);
-    request2.send()
-    console.log(request2.responseType);
-    var results2 = JSON.parse(request2.responseText);
-    console.log(results2[hu]);
-}
 
 function finder() {
-    var request2 = new XMLHttpRequest();
-    request2.open("GET", "products.json", false);
-    request2.send();
-    console.log(request2.responseType);
-    var results2 = JSON.parse(request2.responseText);
+    var dataList = document.getElementById("productos");
+    var input = document.getElementById("productos");
+    var results = CargarJsonLocal("products.json")
     var filtered_array = [];
-
-    for (const result in results2) {        
-        results2[result].forEach(element => {
+    function huhu(params) {
+      console.log("kokoko");
+    }
+    for (const result in results) {        
+        results[result].forEach(element => {
             filtered_array.push(element.name)
+            // Create a new <option> element.
+            var option = document.createElement('option');
+            option.value = element.name;
+            option.onclick = "huhu()";
+            dataList.appendChild(option);
         });
-        // if (results2.hasOwnProperty(result)) {
+        
+        // if (results.hasOwnProperty(result)) {
         //     filtered_array.push(results.filter(obj => obj.name[0] == presskey))
         // }
     }
